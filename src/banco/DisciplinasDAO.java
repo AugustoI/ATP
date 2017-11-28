f2c57c4d29599d287a597986e820158162defc70
AugustoI <gustavo.augusto.pires@hotmail.com>
2017-11-28 19:31:20 -0200

    DisciplinasDAO

---
+++ src/banco/DisciplinasDAO.java
@@ -0,0 +1,17 @@
+package banco;
+
+/*
+ * @author GUSTAVO
+ */
+public class DisciplinasDAO {
+    private String Disciplinas;
+
+    public String getDisciplinas() {
+        return Disciplinas;
+    }
+
+    public void setDisciplinas(String Disciplinas) {
+        this.Disciplinas = Disciplinas;
+    }
+    
+}
