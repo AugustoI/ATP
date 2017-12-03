ab92ae54bfd6914d86c4279c792b2837e71fe0c7
AugustoI <gustavo.augusto.pires@hotmail.com>
2017-12-03 14:42:50 -0200

    ooo

--- src/banco/Banco.java
+++ src/banco/Banco.java
@@ -3,7 +3,9 @@ package banco;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  *
@@ -13,4 +15,10 @@ public class Banco {
     public Connection conectar() throws SQLException{        
         return DriverManager.getConnection("jdbc:mysql://localhost/quest","root","061099");        
     }     
+    public ResultSet executaSQL(String SQL) throws SQLException{
+        Statement con = conectar().createStatement();
+        ResultSet dados;
+        dados = con.executeQuery(SQL);        
+        return (dados);        
+    }
 }
